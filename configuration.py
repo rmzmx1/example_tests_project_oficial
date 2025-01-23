2fdedc054167edb8e8358a6e831515305208cfe8
Ramiro Alejandro Ramirez Cuellar <ramiroramirez@Mac-mini-de-Ramiro.local>
2025-01-23 17:08:25 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-f41e437c-f6b2-445a-8ebd-e4a150be4e28.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
